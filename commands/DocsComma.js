cf12896381cc03cea2fda7e425e59ad3878b8773
Zack Campbell <zajrik@gmail.com>
2016-10-24 04:11:32 -0500

    Allow other docs prefixes

---
+++ commands/DocsComma.js
@@ -0,0 +1,27 @@
+let Command = require('yamdbf').Command;
+
+exports.default = class Docs extends Command
+{
+	constructor(bot)
+	{
+		super(bot, {
+			name: 'docs,',
+			description: 'Provides information from YAMDBF documentation',
+			usage: 'docs <arg>',
+			extraHelp: '',
+			group: 'base'
+		});
+	}
+
+	action(message, args, mentions, original) // eslint-disable-line no-unused-vars
+	{
+		try
+		{
+			return this.bot.commands.get('docs:').action(message, args, mentions, original);
+		}
+		catch (err)
+		{
+			return console.log(err);
+		}
+	}
+};
